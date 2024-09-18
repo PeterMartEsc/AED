75299df0a0b46d752a39ba3d50f103e93428ec14
PeterMartEsc <petermartesc@gmail.com>
2024-09-18 19:11:02 +0100

    feat: Se empieza un ejercicio propuesto

---
+++ ejercicios-propuestos/ejProp1/formulario.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="author" content="Pedro Martin Escuela">
+    <title>Formulario</title>
+</head>
+<body>
+    <form action="formularioResolver.php" method="post">
+        <label for="">Introduzca el texto:</label>
+        <input type="text" id="texto">
+        <input type="submit" id="submit">
+    </form>
+</body>
+</html>
+
+<!--
+    id: JavaScript
+    name: php
+-->
---
+++ ejercicios-propuestos/ejProp1/formularioResolver.php
@@ -0,0 +1,5 @@
+<?php
+
+echo $numdescomponer = $_REQUEST["numdescomponer"];
+
+?>
