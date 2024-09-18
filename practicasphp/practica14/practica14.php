cca15781931387a765fe4530f6e648455cc08bbb
PeterMartEsc <petermartesc@gmail.com>
2024-09-18 17:58:49 +0100

    feat: Se arregla la practica 14

--- practicasphp/practica14/practica14.php
+++ practicasphp/practica14/practica14.php
@@ -1,11 +1,19 @@
 <?php
 
     for($i=0;$i<10;$i++){
-        
+        $variable = "dato";
+        ${$variable.$i} = $i;
+        //echo ${"dato".$i};
     }
 
     echo "<br> $dato3 ";
     echo "<br> $dato8 ";
 ?>
 
+<!--
+    Toma el código anterior e introduce una expresión “variable de variables” que
+    permita definir las variables: $dato0, $dato1, …, $dato9 Cada una de ellas con el valor
+    correspondiente: 0, 1,…,9
 
+
+-->
