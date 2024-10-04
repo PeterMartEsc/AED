2fe5aface2e920328728d116a6c7aba84a5b5671
PeterMartEsc <petermartesc@gmail.com>
2024-10-04 16:12:44 +0100

    feat: Cambios menores en la practica 3

---
+++ 1erTrimestre/laravel/pruebas/dossier/pr12/pr12.php
@@ -0,0 +1,5 @@
+<!--
+    Ubicar imágenes en la carpeta descrita para las imágenes que quieras mostrar
+    ( mínimo 5 ). Hacer que se visualicen en el navegador las imágenes en nuestra vista
+    nota: Poner el renderizado que se considere más apropiado
+-->
--- 1erTrimestre/laravel/pruebas/proyectoTrabajar/app/Http/Controllers/Controller_Pr07.php
+++ 1erTrimestre/laravel/pruebas/proyectoTrabajar/app/Http/Controllers/Controller_Pr07.php
@@ -8,6 +8,7 @@ class Controller_Pr07 extends Controller
     public function numPrimos(){
 
         $coleccion = collect([1,2,3,5,7,11,13,17,19]);
+        //Pr08
         $hora = date('H:i:s');
         return view('listarPrimos_Pr07', ['coleccion' => $coleccion, 'hora' => $hora]);
     }
