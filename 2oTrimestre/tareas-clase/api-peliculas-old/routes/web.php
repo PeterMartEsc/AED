d631b775ab95201de0d0dcb453ffd533fac1475b
PeterMartEsc <petermartesc@gmail.com>
2024-12-17 17:57:40 +0000

    feat(api-peliculas): Se deja de trabajar en el proyecto api peliculas, pasa a nombrarse old, y se crea uno nuevo con el nombre anterior

--- 2oTrimestre/tareas-clase/api-peliculas-old/routes/web.php
+++ 2oTrimestre/tareas-clase/api-peliculas-old/routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,3 +17,20 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get("/basica", function(Request $request){
+
+    $authHeader = $request->header('Authorization');
+    if(!$authHeader){
+        return response('Unauthorized', 401)
+                ->header('WWW-Authenticate', 'Basic realm="Acceso al recurso protegido"');
+
+    }else {
+        $decoded = base64_decode(substr($authHeader, 6));
+        echo $decoded;
+        die();
+
+    }
+
+});
+
