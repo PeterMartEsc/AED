955e2a5c8d8bfe06c9ad8855f2c5026238710b03
PeterMartEsc <petermartesc@gmail.com>
2025-01-24 20:29:37 +0000

    feat(api instituto sec): Se empieza la pi de instituto securizada

---
+++ 2oTrimestre/tareas-clase/InstitutoSec/src/main/java/es/iespuertodelacruz/pme/institutosec/InstitutoSecApplication.java
@@ -0,0 +1,13 @@
+package es.iespuertodelacruz.pme.institutosec;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InstitutoSecApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InstitutoSecApplication.class, args);
+	}
+
+}
---
+++ 2oTrimestre/tareas-clase/InstitutoSec/src/test/java/es/iespuertodelacruz/pme/institutosec/InstitutoSecApplicationTests.java
@@ -0,0 +1,13 @@
+package es.iespuertodelacruz.pme.institutosec;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InstitutoSecApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
