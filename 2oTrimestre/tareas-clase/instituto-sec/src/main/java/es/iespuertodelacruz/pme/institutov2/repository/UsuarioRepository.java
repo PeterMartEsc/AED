ef57fff7266e9f02e9e16321a9efedde5db4538a
PeterMartEsc <petermartesc@gmail.com>
2025-01-27 11:43:52 +0000

    feat(instituto-sec): Creacion de las clases

---
+++ 2oTrimestre/tareas-clase/instituto-sec/src/main/java/es/iespuertodelacruz/pme/institutov2/repository/UsuarioRepository.java
@@ -0,0 +1,2 @@
+package es.iespuertodelacruz.pme.institutov2.repository;public interface UsuarioRepository {
+}
---
+++ 2oTrimestre/tareas-clase/instituto-sec/src/main/java/es/iespuertodelacruz/pme/institutov2/service/AsignaturaService.java
@@ -0,0 +1,2 @@
+package es.iespuertodelacruz.pme.institutov2.service;public class AsignaturaService {
+}
