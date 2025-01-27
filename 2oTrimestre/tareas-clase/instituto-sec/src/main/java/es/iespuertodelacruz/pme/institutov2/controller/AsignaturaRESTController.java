ac267d647ff60cf347fbdda57b416318d8d67c79
PeterMartEsc <petermartesc@gmail.com>
2025-01-27 19:35:26 +0000

    feat(instituto-sec): Se empieza a trabajar en el controller de Asignaturas y se crean los dto

---
+++ 2oTrimestre/tareas-clase/instituto-sec/src/main/java/es/iespuertodelacruz/pme/institutov2/controller/AsignaturaRESTController.java
@@ -0,0 +1,4 @@
+package es.iespuertodelacruz.pme.institutov2.controller;
+
+public class AsignaturaRESTController {
+}
---
+++ 2oTrimestre/tareas-clase/instituto-sec/src/main/java/es/iespuertodelacruz/pme/institutov2/dto/AsignaturaDTOEntrada.java
@@ -0,0 +1,4 @@
+package es.iespuertodelacruz.pme.institutov2.dto;
+
+public record AsignaturaDTOEntrada() {
+}
