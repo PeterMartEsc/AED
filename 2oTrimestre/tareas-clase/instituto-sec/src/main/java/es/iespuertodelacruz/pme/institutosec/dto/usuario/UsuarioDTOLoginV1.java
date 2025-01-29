2e0a245bda3b49fadd261b74597fc7a6e4c8bb5a
PeterMartEsc <petermartesc@gmail.com>
2025-01-29 22:20:29 +0000

    feat(instituto-sec): Se crean los archivos de configuración de login y register

--- 2oTrimestre/tareas-clase/instituto-sec/src/main/java/es/iespuertodelacruz/pme/institutosec/dto/usuario/UsuarioDTOLoginV1.java
+++ 2oTrimestre/tareas-clase/instituto-sec/src/main/java/es/iespuertodelacruz/pme/institutosec/dto/usuario/UsuarioDTOLoginV1.java
@@ -1,6 +1,6 @@
 package es.iespuertodelacruz.pme.institutosec.dto.usuario;
 
-public record UsuarioDTOEntradaV1(
+public record UsuarioDTORegisterV1(
         //int id,
         String nombre,
         String correo,
---
+++ 2oTrimestre/tareas-clase/instituto-sec/src/main/java/es/iespuertodelacruz/pme/institutosec/dto/usuario/UsuarioDTORegisterV1.java
@@ -0,0 +1,9 @@
+package es.iespuertodelacruz.pme.institutosec.dto.usuario;
+
+public record UsuarioDTORegisterV1(
+        //int id,
+        String nombre,
+        String correo,
+        String password
+) {
+}
